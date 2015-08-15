e950c178fa2f4163b35a6838245abeed065ad241
taylor.foss <taylorfoss89@gmail.com>
2015-08-15 10:53:02 -0700

    Added ->new submethod to Coffee

--- Brew/Coffee.pm
+++ Brew/Coffee.pm
@@ -1,32 +1,36 @@
 #! /usr/bin/perl
-
+package Brew::Coffee;
 use warnings;
 use strict;
 
 @ISA = qw( Brew );
 
-package Brew::Coffee;
+sub new
+{
+    my ($class, %args) = @_;
+    #$class->_incr_count();
+    bless{
+            _barista            => $arg{barista},
+            _bitterness         => $arg{bitterness},
+            _brew_time          => $arg{brew_time},
+            _coffee_bean        => $arg{coffee_bean},
+            _coffee_quantity    => $arg{coffee_quantity},
+            _coffee_grind_size  => $arg{coffee_grind_size},
+            _date               => $arg{date},
+            _device             => $arg{device},
+            _flavors            => $arg{flavors},
+            _mouthfeel          => $arg{mouthfeel},
+            _rating             => $arg{rating},
+            _strength           => $arg{strength},
+            _successful_brew    => $arg{strength},
+            _sweetness          => $arg{sweetness},
+            _water_quantity     => $are{water_quantity},
+            _water_temperature  => $arg{water_temperature},
+        }, $class;
+}
 
-# What attributes with the coffee brewing methods inherit from their superclass, brew?
-# water_temperature
-# water_quantity
-# coffee_quantity
-# coffee_grind_size
-# time
-# date?
-# device
-# coffee_type -- does coffee need to have its own class -- with attributes -- country, flavors, roast?
-# barista
-# rating
-# sweetness
-# mouthfeel
-# bitterness
-# strength
-# brew_time
-# bloomed
-# stirred
-# successful_brew
-# flavors
+
+
 
 
 
