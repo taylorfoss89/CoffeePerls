e84c974ea100c96aa1abfc9ed4ddb78ad4f749c7
taylor.foss <taylorfoss89@gmail.com>
2015-08-23 12:25:58 -0700

    Added a good deal more to the UI, including several screens, screen navigation functionality, actions memory, and cleaned up the display of each screen to the user for improved use and cleanliness

--- UI.pm
+++ UI.pm
@@ -6,7 +6,6 @@ use strict;
 use Term::UI;
 use Term::ReadLine;
 
-
 # This defines the questions that are asked of the User and how, through decisions,
 # the user will be guided through entering the appropriate information for recording
 # a new brew to the database. This will also define how the user can then access said
@@ -33,10 +32,41 @@ use Term::ReadLine;
 #
 #
 
-my $barista;    # Setting my user, otherwise known as the barista who made coffee 
-my $action;     # not sure, may need to know what my current action is to know who and what I am 
-my $page;       # Setting current page
-my $last_page;  # Setting previous page for using "return to last page"
+my $barista;                # Setting my user, otherwise known as the barista who made coffee 
+my @actions = ("Login");    # Plan is to use this to store the movements made through the UI for intelligently returning to previous pages
+my $page;                   # Setting current page
+my $last_page;              # Setting previous page for using "return to last page"
+my $continuing = 1;         # Program continues to run as long as this is equal to 1. Setting to 0 will shut everything down
+
+my %translations =          # Defining the subroutines that handle each action from the user. May change, but have not found a better idea yet
+(
+    "Login"                     =>  "login_prompt",
+    "Return to previous page"   =>  "return",
+    "Return to main menu"       =>  "main_menu",
+    "Record a brew"             =>  "create_brew",
+    "Logout"                    =>  "logout",
+);
+
+# Use a looping mechanism to progress through the UI
+# iter1 -> login as Taylor -> iter2 -> selected action -> iter3 -> selected action ...
+# Not sure how other programs have handled such things, will have to look into it
+
+while ( $continuing )
+{
+    no strict "refs";
+
+    print "\033[2J";    # Clears the terminal screen for aesthetics and ease of use
+
+    my $action = $translations{$actions[-1]}();
+    push @actions, $action; #$translations{$actions[-1]};
+
+
+
+};
+
+
+
+
 
 
 
@@ -45,8 +75,10 @@ my $last_page;  # Setting previous page for using "return to last page"
 
 sub login_prompt
 {
-    print "Welcome, barista!\n\n\n\n";
+    print "\n\n\n\nWelcome, barista!\n\n\n\n";
     print "Before we begin, I must know who you are.\n\n";
+
+    my $term = Term::ReadLine->new('login');
 
     $barista = $term->get_reply(
                 prompt  => "Who am I assisting today?",
@@ -54,6 +86,7 @@ sub login_prompt
                 default => 'Taylor',
             );
 
+    return "Return to main menu";
 };
 
 sub main_menu
@@ -61,5 +94,37 @@ sub main_menu
     print "\n\n\n
            MAIN MENU\n
            ===================\n\n\n";       #need to utilize sprintf here for proper formatting, will worry about later
-    
+    print "Welcome $barista, what can I help you with?\n\n";
 
+    my $term = Term::ReadLine->new('main menu');
+
+    $term->get_reply(
+        prompt  =>  "What can I help you with?",
+        choices =>  [   'Record a brew',
+                        'Tell me about an old brew',
+                        'Show me the best brew I can make',
+                        'Logout',
+                    ],
+        default => 'Record a brew',
+    );
+
+};
+
+sub create_brew
+{
+    print "recording your first brew!!!!\n\n";
+    $continuing--;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+1;
